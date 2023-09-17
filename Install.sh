9970f1e79a96a21e2f9beeffb8bcea6633923bed
VairamuthuBaskar <65724759+VairamuthuBaskar@users.noreply.github.com>
2023-09-17 11:16:03 +0530

    Update Install.sh
    Commited for WebHookTest Jenkins Job

--- Install.sh
+++ Install.sh
@@ -1,4 +1,4 @@
-# Build Function Based Shell Script for all software installations and Maven execution
+# Build Function Based Shell Script for all software installations and Maven executions
 
 
 #!/bin/bash
